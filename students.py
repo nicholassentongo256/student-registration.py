b3fda84f7632b23d758f9764b4ef4e15af106eb8
Ssentongo Nicholas <139561412+nicholassentongo256@users.noreply.github.com>
2024-10-05 07:47:18 +0300

    Add files via upload

--- students.py
+++ students.py
@@ -1,16 +1,18 @@
 class Student:
-    def __init__(self, name, reg_number):
+    def __init__(self, name, reg_number, course):
         self.name = name
         self.reg_number = reg_number
+        self.course = course
 
     def display_info(self):
         print(f"Student Name: {self.name}")
         print(f"Registration Number: {self.reg_number}")
+        print(f"course: {self.course}")
 
-student1 = Student("Nicho Sssentongo", "M23B13/013")
-student2 = Student("Bash Mickey", "M23B13/019")
-student3 = Student("Hajarah Ssentongo", "M23B13/016")
-student4 = Student("kemi Musa", "M23B13/015")
+student1 = Student("Nicho Sssentongo", "M23B13/013","Computer Science")
+student2 = Student("Bash Mickey", "M23B13/019", "Information technology")
+student3 = Student("Hajarah Ssentongo", "M23B13/016", "Data analysis")
+student4 = Student("kemi Musa", "M23B13/015","Computer Science")
 
 student1.display_info()
 print()  
